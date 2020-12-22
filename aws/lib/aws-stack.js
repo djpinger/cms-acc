403d70ac100a4a21d376100501275beff7cb5b61
TheMerryMarlin <dam134@case.edu>
2020-12-22 15:12:21 -0600

    add s3 event notifications to lambda, add delegator lambda. add some extra comments to workflow, add lambda dep

--- aws/lib/aws-stack.js
+++ aws/lib/aws-stack.js
@@ -1,6 +1,9 @@
 const cdk = require('@aws-cdk/core');
 const s3 = require('@aws-cdk/aws-s3');
 const s3n = require('@aws-cdk/aws-s3-notifications');
+const lambda = require('@aws-cdk/aws-lambda');
+const path = require('path');
+const { LambdaDestination } = require('@aws-cdk/aws-s3-notifications');
 
 class AwsStack extends cdk.Stack {
   /**
@@ -26,6 +29,34 @@ class AwsStack extends cdk.Stack {
      *    Again could also end up in a db. dynamo is "free" forever, probably our starting candidate if
      *    need one. That being said stateless may be better for now
      */
+    // may be worth having a lambda that just ingests s3 put events and then delegates, probably eaiser
+    //for the collab code stuff as we can mess with input formats and make results prettier
+
+    //bucket to hold results files, populated manually with our agreed upon 'directory' structure 
+    //(s3 is technically flat and just uses naming to create the illusion of directories)
+    const resultsBucket = new s3.Bucket(this, `${id}-results-bucket`, {
+      blockPublicAccess: {
+        blockPublicAcls: true, 
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true
+      },
+      bucketName: 'cms-results-file-bucket',
+      publicReadAccess: false
+    });
+
+    //lambda to ingest new files and kick off the proper processing lambdas
+    //basically driver rating always and champ points as needed
+    const delegatorLambda = new lambda.Function(this, `${id}-delegator-lambda`, {
+      code: lambda.Code.fromAsset(path.join(__dirname, '..','delegator','src')),
+      handler: 'delegatorLambda.handler',
+      runtime: lambda.Runtime.NODEJS_12_X
+    });
+
+    //this glues our bucket to the delegator. New files kick off the function
+    const resultsFileEvent = new LambdaDestination(delegatorLambda);
+    resultsBucket.addEventNotification(s3.EventType.OBJECT_CREATED, resultsFileEvent);
+
   }
 }
 
